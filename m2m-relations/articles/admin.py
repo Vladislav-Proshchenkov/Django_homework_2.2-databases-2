63e58416b88e2a6aeb842e28b48b21693f9cec9b
Vladislav Proshchenkov <vladislove.pro@gmail.com>
2024-09-08 13:14:40 +0300

    m2m-relations

--- m2m-relations/articles/admin.py
+++ m2m-relations/articles/admin.py
@@ -1,8 +1,35 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
 
-from .models import Article
+from .models import Article, Scopes
 
+
+class ScopesInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        for form in self.forms:
+            # В form.cleaned_data будет словарь с данными
+            # каждой отдельной формы, которые вы можете проверить
+            # form.cleaned_data
+            # вызовом исключения ValidationError можно указать админке о наличие ошибки
+            # таким образом объект не будет сохранен,
+            # а пользователю выведется соответствующее сообщение об ошибке
+            if 'tag' not in form.cleaned_data:
+                raise ValidationError('Нет тега')
+
+            if 'is_main' not in form.cleaned_data:
+                raise ValidationError('Нет главного тега')
+
+
+            # raise ValidationError('Тут всегда ошибка')
+        return super().clean()  # вызываем базовый код переопределяемого метода
+
+
+class ScopeInline(admin.TabularInline):
+    model = Scopes
+    formset = ScopesInlineFormset
+    extra = 0
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    pass
+    inlines = [ScopeInline]
--- m2m-relations/articles/models.py
+++ m2m-relations/articles/models.py
@@ -9,8 +9,32 @@ class Article(models.Model):
     image = models.ImageField(null=True, blank=True, verbose_name='Изображение',)
 
     class Meta:
-        verbose_name = 'Статья'
-        verbose_name_plural = 'Статьи'
+        verbose_name = 'Статьи'
 
     def __str__(self):
         return self.title
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=256, verbose_name='Название')
+    articles = models.ManyToManyField(Article, related_name='Теги', through='Scopes')
+
+    class Meta:
+        verbose_name = 'Раздел'
+        verbose_name_plural = 'Разделы'
+
+    def __str__(self):
+        return self.name
+
+
+class Scopes(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='scopes')
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    is_main = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name = 'Тематика'
+        verbose_name_plural = 'Тематики'
+
+    def __str__(self):
+        return f'{self.tag}'
